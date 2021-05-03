607aa99fd7bbb5c6285ba57772619da2420ce7fa
vishaldeshnukh <51355426+vishaldeshnukh@users.noreply.github.com>
2021-05-03 10:09:34 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,4 +1,6 @@
 const daysToCheck = 10
 const PINCODE = 431134
 const AGE = 51
-module.exports = {daysToCheck, PINCODE, AGE}
+const email = ""
+const password = ""
+module.exports = {daysToCheck, PINCODE, AGE, email, password}
